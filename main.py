de2db28f3d6183d986452acb02044baeed649c17
Skeyling1 <skeyling1@gmail.com>
2024-09-14 16:44:48 +0300

    second_changes

--- main.py
+++ main.py
@@ -3,15 +3,39 @@ import random
 
 
 pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HIGHT = 600
 
 
-play_screen = pygame.display((SCREEN_HIGHT, SCREEN_WIDTH))
+
+play_screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Matryoshka Invasion')
+icon = pygame.image.load("img/pngwing.com.png")
+pygame.display.set_icon(icon)
+
+
+matryoshka = pygame.image.load("img/pngwing.com.png")
+matryoshka_height = 80
+matryoshka_width = 80
+
+matryoshka_x = random.randint(0, 800-matryoshka_width)
+matryoshka_y = random.randint(0, 600-matryoshka_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+
 
 running = True
 while running:
-    pass
+    play_screen.fill(color)
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
+        if event == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if matryoshka_x < mouse_x and mouse_x < matryoshka_x + matryoshka_width and matryoshka_y < mouse_y and mouse_y < matryoshka_y + matryoshka_height:
+                pass
+
+    pygame.display.flip()
 
 
 
