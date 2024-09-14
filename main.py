b75348d08cb122a2ec83f1a4810ae5682ebfe112
Skeyling1 <skeyling1@gmail.com>
2024-09-14 15:09:42 +0300

    first_structure

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+
+
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HIGHT = 600
+
+
+play_screen = pygame.display((SCREEN_HIGHT, SCREEN_WIDTH))
+
+running = True
+while running:
+    pass
+
+
+
+
+
+
+pygame.quit()
